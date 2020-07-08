8f558a0680948ca8a1b86cb0fab6ba1398174a6f
Omar Faruk <43259457+OMoF@users.noreply.github.com>
2020-07-08 21:38:20 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+window.addEventListener('load', () => {
+    document.querySelector('.preloader').style.display = 'none'
+})
+
+let menuNodes = document.querySelectorAll('.nav .nav-menus__menu');
+
+menuNodes.forEach(node => {
+    node.addEventListener('click', () => {
+        let check = document.querySelector('.nav .nav-menus__menu.active')
+        if (check){
+            check.classList.remove('active')
+        }
+        node.classList.add('active')
+        document.querySelector('header').classList.toggle('open')
+
+    })
+
+})
+
+
+document.querySelector('.home-down__btn').addEventListener('click',() => {
+    const {hash,origin} = window.location
+        window.location = origin + "/" + '#about';
+})
+
+document.querySelector('#mobi-navBtn').addEventListener('click',() => {
+    document.querySelector('header').classList.toggle('open')
+})
