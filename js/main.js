8c72d5f89a16e14843c5f9abfb021c98bd18be93
Omar Faruk <43259457+OMoF@users.noreply.github.com>
2020-07-08 21:58:39 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,28 +1,26 @@
-window.addEventListener('load', () => {
-    document.querySelector('.preloader').style.display = 'none'
-})
+window.addEventListener("load", () => {
+  document.querySelector(".preloader").style.display = "none";
+});
 
-let menuNodes = document.querySelectorAll('.nav .nav-menus__menu');
+let menuNodes = document.querySelectorAll(".nav .nav-menus__menu");
 
-menuNodes.forEach(node => {
-    node.addEventListener('click', () => {
-        let check = document.querySelector('.nav .nav-menus__menu.active')
-        if (check){
-            check.classList.remove('active')
-        }
-        node.classList.add('active')
-        document.querySelector('header').classList.toggle('open')
+menuNodes.forEach((node) => {
+  node.addEventListener("click", () => {
+    let check = document.querySelector(".nav .nav-menus__menu.active");
+    if (check) {
+      check.classList.remove("active");
+    }
+    node.classList.add("active");
 
-    })
+    document.querySelector("header").classList.toggle("open");
+  });
+});
 
-})
+document.querySelector(".home-down__btn").addEventListener("click", () => {
+  const { hash, origin } = window.location;
+  window.location = origin + "/" + "#about";
+});
 
-
-document.querySelector('.home-down__btn').addEventListener('click',() => {
-    const {hash,origin} = window.location
-        window.location = origin + "/" + '#about';
-})
-
-document.querySelector('#mobi-navBtn').addEventListener('click',() => {
-    document.querySelector('header').classList.toggle('open')
-})
+document.querySelector("#mobi-navBtn").addEventListener("click", () => {
+  document.querySelector("header").classList.toggle("open");
+});
